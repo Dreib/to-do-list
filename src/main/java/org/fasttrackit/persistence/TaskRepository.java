729763dbe87c65a71c94254b6350b872e9fda2af
Andrei <andreibuzas@gmail.com>
2020-07-23 21:25:09 +0300

    added delete task method in task repository

--- src/main/java/org/fasttrackit/persistence/TaskRepository.java
+++ src/main/java/org/fasttrackit/persistence/TaskRepository.java
@@ -16,15 +16,23 @@ public class TaskRepository {
 
         //try-with-resources
         try (PreparedStatement preparedStatement = DatabaseConfiguration.getConnection()
-                .prepareStatement(sql);) {
+                .prepareStatement(sql)) {
             preparedStatement.setString(1, request.getDescription());
             preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
 
             preparedStatement.executeUpdate();
         }
+    }
 
+    public void deleteTask(long id) throws SQLException {
+        String sql = "DELETE FROM task WHERE id = ?";
 
+        try(PreparedStatement preparedStatement = DatabaseConfiguration.getConnection()
+                .prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
 
+            preparedStatement.executeUpdate();
+        }
     }
 
 }
