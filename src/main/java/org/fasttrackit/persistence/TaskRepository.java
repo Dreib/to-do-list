9d9350312fe8563e79ff139758aeaecc66c57560
Andrei <andreibuzas@gmail.com>
2020-07-30 19:02:05 +0300

    added get tasks method in TaskRepository

--- src/main/java/org/fasttrackit/persistence/TaskRepository.java
+++ src/main/java/org/fasttrackit/persistence/TaskRepository.java
@@ -1,12 +1,13 @@
 package org.fasttrackit.persistence;
 
 import org.fasttrackit.config.DatabaseConfiguration;
+import org.fasttrackit.domain.Task;
 import org.fasttrackit.transfer.CreateTaskRequest;
 import org.fasttrackit.transfer.UpdateTaskRequest;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 //DAO (Data Access Object)
 public class TaskRepository {
@@ -46,6 +47,29 @@ public class TaskRepository {
 
             preparedStatement.executeUpdate();
         }
+    }
+
+    public List<Task> getTasks() throws SQLException {
+        String sql = "SELECT id, description, deadline, done FROM task";
+
+        List<Task> tasks = new ArrayList<>();
+
+        try(Statement statement = DatabaseConfiguration.getConnection().createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while(resultSet.next()) {
+                Task task = new Task();
+
+                task.setId(resultSet.getLong("id"));
+                task.setDescription(resultSet.getString("description"));
+                task.setDeadline(resultSet.getDate("deadline").toLocalDate());
+                task.setDone(resultSet.getBoolean("done"));
+
+                tasks.add(task);
+            }
+        }
+
+        return tasks;
     }
 
 }
